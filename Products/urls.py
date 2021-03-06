92f7cf389bb5f1b8cc97f9ddb2bbff7e95c39720
dennis-simiyu <simiyudennis142@gmail.com>
2021-03-06 15:41:27 -0600

    Additional features

--- Products/urls.py
+++ Products/urls.py
@@ -6,7 +6,7 @@ app_name='Products'
 urlpatterns=[
     path('', views.home_pageview, name='home_page'),
     path('products/',
-        views.products_list, name='product_list'),
+        views.products_list, name='products_list'),
     path('products/product-detail/',
         views.product_detail, name='product-detail'),
 ]
